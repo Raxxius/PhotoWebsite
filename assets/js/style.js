1f923c6454afeb2a26b55a5bfefc586be66f574b
Raxxius <Raxxman@gmail.com>
2021-11-06 10:53:00 +0000

    add JS for index slider

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,3 +1,16 @@
-function slider () {
-    
+
+var slideIndex = 0;
+showIndexSlides();
+
+function showIndexSlides() {
+
+  let i = 0;
+  let slides = document.getElementsByClassName("indexSlideImg");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showIndexSlides, 500); // Change image every 2 seconds
 }
