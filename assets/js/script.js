66eaa040e8aaeabe44b49d12a43d8348280e7550
Raxxius <Raxxman@gmail.com>
2021-11-09 10:03:37 +0000

    moved showIndexSlides function to a seperate

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,12 @@ function selectIndexSlides() {
     document.getElementById("indexSlideshow").appendChild(var2);
   }
     showIndexSlides()
+}
+
+/**
+ * This is the index slideshow for the Index page, this function takes five random images from the galleries and displays them 
+ */
+
     function showIndexSlides() {
     let z = 0;
     let slides = document.getElementsByClassName("indexSlideImg");
@@ -50,26 +56,8 @@ function selectIndexSlides() {
     slides[slideIndex-1].style.display = "block";  
     setTimeout(showIndexSlides, 5000); // Change image every 5 seconds
   }
-}
-
-/**
- * This is the index slideshow for the Index page, this function takes five random images from the galleries and displays them 
 
 
-function showIndexSlides() {
-
-  let i = 0;
-  let slides = document.getElementsByClassName("indexSlideImg");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  slides[slideIndex-1].style.display = "block";  
-  setTimeout(showIndexSlides, 5000); // Change image every 5 seconds
-}
-
- */
 
 /**
  * These functions hides or displays images in the gallery.html depending upon the checkboxes selected in the gallery
