1faaec36e42ef99a05307fc8db67ab034e653481
Raxxius <Raxxman@gmail.com>
2021-11-10 02:18:16 +0000

    push for the night

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-
-
-
+let slideIndex = 0;
 /**
  * This function selects 5 random landscape images for the index pages slider to used.
  * It uses the first array of images from the gallery and a random number generator that will select one from random and add it to the new array. 
@@ -17,18 +15,17 @@ function selectIndexSlides() {
   let indexSlides = [];
 
   for (i = 0; i < 5; i++) {
-     let num = Math.floor(Math.random() * slideArray.length);
-      indexSlides.push(slideArray[num]);
-      slideArray.splice(num, 1)
-    }
+    let num = Math.floor(Math.random() * slideArray.length);
+    indexSlides.push(slideArray[num]);
+    slideArray.splice(num, 1)
+  }
 
-/**
- * This section creates the image HTML to be used by the index
- */
-  
+  /**
+   * This section creates the image HTML to be used by the index
+   */
+
   let x = 0;
   for (x = 0; x < 5; x++) {
-    let var1 = 'indexSlideShow'+[x]+'';
     let var2 = document.createElement("img");
     let var3 = `assets/images/${indexSlides[x]}`;
     var2.className += ("indexSlideImg fade");
@@ -36,26 +33,24 @@ function selectIndexSlides() {
     var2.style.display += "none";
     document.getElementById("indexSlideshow").appendChild(var2);
   }
-    showIndexSlides()
+  showIndexSlides()
 }
 
 /**
  * This is the index slideshow for the Index page, this function takes five random images from the galleries and displays them 
  */
 
-    function showIndexSlides() {
-    let slideIndex = 0;
-    let z = 0;
-    let slides = document.getElementsByClassName("indexSlideImg");
-    console.log(slides[0]);
-      for (z = 0; z < slides.length; z++) {
-        slides[z].style.display = "none";  
-      }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    slides[slideIndex-1].style.display = "block";  
-    setTimeout(showIndexSlides, 5000); // Change image every 5 seconds
-  }
+ function showIndexSlides() {
+  let z = 0;
+  let slides = document.getElementsByClassName("indexSlideImg");
+    for (z = 0; z < slides.length; z++) {
+      slides[z].style.display = "none";  
+    }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showIndexSlides, 5000); // Change image every 5 seconds
+}
 
 
 
@@ -66,11 +61,12 @@ function selectIndexSlides() {
 /**
  * The Core loop - removes images that are not tagged via the checkboxloop function
  */
- function displayNoneLoop() {
+
+function displayNoneLoop() {
   let GalleryVar = document.getElementsByClassName(arguments[0]);
   let a = 0;
   for (a = 0; a < GalleryVar.length; a++) {
-      GalleryVar[a].style.display ="none";
+    GalleryVar[a].style.display = "none";
   }
 }
 
@@ -79,16 +75,16 @@ function selectIndexSlides() {
  */
 
 function GalleryDisplay() {
-let i = 0;
-let images = document.getElementsByClassName("galleryThumb");
-for (i = 0; i < images.length; i++) {
-images[i].style.display = "block";
-}
-checkboxloop("checkbox2018", "LCC2018")
-checkboxloop("checkbox2019", "LCC2019")
-checkboxloop("checkbox2021", "LCC2021")
-checkboxloop("portraitcheckbox", "portrait")
-checkboxloop("landscapecheckbox", "landscape")
+  let i = 0;
+  let images = document.getElementsByClassName("galleryThumb");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "block";
+  }
+  checkboxloop("checkbox2018", "LCC2018")
+  checkboxloop("checkbox2019", "LCC2019")
+  checkboxloop("checkbox2021", "LCC2021")
+  checkboxloop("portraitcheckbox", "portrait")
+  checkboxloop("landscapecheckbox", "landscape")
 }
 
 /**
@@ -96,10 +92,8 @@ checkboxloop("landscapecheckbox", "landscape")
  */
 
 function checkboxloop() {
-let checkbox2018 = document.getElementById(arguments[0]);
-if (checkbox2018.checked !== true) {
-  displayNoneLoop(arguments[1]);
+  let checkbox2018 = document.getElementById(arguments[0]);
+  if (checkbox2018.checked !== true) {
+    displayNoneLoop(arguments[1]);
   }
 }
-
-
